dd463189a03ec594d98b684fd848cc0520f8a718
Jilong Liao <jliao2@utk.edu>
2013-04-08 18:17:22 -0400

    cleanup the data input

--- external/3-sat-data.cc
+++ external/3-sat-data.cc
@@ -14,27 +14,30 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 
 using namespace std;
 
-const int NUM_CASES = 1000000;
 
-
-int main () {
+int main (int argc, char *argv[]) {
+	int NumberOfCases = 1000000;
+	if (argc == 3 && strcmp(argv[1], "-n") == 0)
+		NumberOfCases = atoi(argv[2]);
 	FILE *fp = fopen("3-sat.in", "w");
 	srand(time(NULL));
-	for (int i = 0; i < NUM_CASES; ++i) {
-		int n = rand() % 20 + 1;
-		int m = 3 * (rand() % 30 + 1);
-		fprintf(fp, "%d,%d", n, m);
-		for (int j = 0; j < m; ++j) {
-			int x = (rand() % 100000) % n + 1;
+	for (int i = 0; i < NumberOfCases; ++i) {
+		int NumberOfVariables = rand() % 20 + 1;
+		int NumberOfExpressions = 3 * (rand() % 30 + 1);
+		fprintf(fp, "%d,%d", NumberOfVariables, NumberOfExpressions);
+		for (int j = 0; j < NumberOfExpressions; ++j) {
+			int x = (rand() % 100000) % NumberOfVariables + 1;
 			if (rand() % 100 >= 50) x = -x;
 			fprintf(fp, ",%d", x);
 		}
 		fprintf(fp, "\n");
-		if (i % 10000 == 0) printf("[%d\%] finished.\n", i/10000);
+		if (i % (NumberOfCases/100+1) == 0) 
+			printf("[%d\%] finished.\n", i/(NumberOfCases/100+1));
 	}
 	fclose(fp);
 	return 0;
---
+++ external/README.md
@@ -0,0 +1,8 @@
+## Data Generator
+The `data generator` is used to create the 3-sat input data. Use the following command to build the generator:
+> $ g++ 3-sat-data.cc -o 3-sat-data
+
+Then you will see the executable file `3-sat-data`, run it with the following style:
+> $ ./3-sat-data -n [number of cases]
+
+After you execute this, you will be able to get a file `3-sat.in` which contains the input data to the solver.
