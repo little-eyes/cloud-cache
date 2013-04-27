79f97813c4eafd28fd99178c049a1a1ca44a8983
Jilong Liao <jliao2@utk.edu>
2013-04-27 15:40:28 +0000

    revise the EC2 domain name

--- src/configure.py
+++ src/configure.py
@@ -6,9 +6,15 @@
 	rely on this file to configure the system.
 '''
 
-MASTER_NODE = ['10.208.175.127']
+#MASTER_NODE = ['10.208.175.127']
+#MASTER_NODE = ['domU-12-31-39-06-A8-71.compute-1.internal']
+MASTER_NODE = ['ec2-54-234-172-153.compute-1.amazonaws.com']
 #MASTER_NODE = ['localhost']
-SLAVE_NODE = ['10.198.129.131', '10.210.205.29', '10.96.37.25', '10.96.245.71']
+#SLAVE_NODE = ['10.198.129.131', '10.210.205.29', '10.96.37.25', '10.96.245.71']
+#SLAVE_NODE = ['domU-12-31-39-10-82-79.compute-1.internal', 'domU-12-31-39-09-CA-D3.compute-1.internal', 'domU-12-31-39-16-26-EF.compute-1.internal', 'domU-12-31-39-16-F6-BD.compute-1.internal']
+SLAVE_NODE = ['ec2-54-225-2-34.compute-1.amazonaws.com', 'ec2-54-242-129-207.compute-1.amazonaws.com', 'ec2-54-225-22-165.compute-1.amazonaws.com', 'ec2-50-16-144-197.compute-1.amazonaws.com']
+PRIVATE_TO_PUBLIC_TABLE = {'10.198.129.131':'ec2-54-225-2-34.compute-1.amazonaws.com', '10.210.205.29':'ec2-54-242-129-207.compute-1.amazonaws.com', '10.96.37.25':'ec2-54-225-22-165.compute-1.amazonaws.com', '10.96.245.71':'ec2-50-16-144-197.compute-1.amazonaws.com'}
+
 
 MASTER_PORT = 12530
 SLAVE_PORT = 12531
