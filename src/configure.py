6e89ad5574288d0b502b7d1116705c3a4c23d3b2
Jilong Liao <jliao2@utk.edu>
2013-05-01 14:07:12 -0400

    ready to test cache-feature on EC2 with pipeline

--- src/configure.py
+++ src/configure.py
@@ -6,20 +6,20 @@
 	rely on this file to configure the system.
 '''
 
-#MASTER_NODE = ['10.215.114.14']
+MASTER_NODE = ['10.215.114.14']
 #MASTER_NODE = ['domU-12-31-39-06-A8-71.compute-1.internal']
 #MASTER_NODE = ['ec2-54-234-172-153.compute-1.amazonaws.com']
 #MASTER_NODE = ['localhost']
-#SLAVE_NODE = ['10.215.197.55', '10.200.251.252', '10.206.118.135', '10.192.169.57']
+SLAVE_NODE = ['10.215.197.55', '10.200.251.252', '10.206.118.135', '10.192.169.57']
 #SLAVE_NODE = ['domU-12-31-39-10-82-79.compute-1.internal', 'domU-12-31-39-09-CA-D3.compute-1.internal', 'domU-12-31-39-16-26-EF.compute-1.internal', 'domU-12-31-39-16-F6-BD.compute-1.internal']
 #SLAVE_NODE = ['ec2-54-225-2-34.compute-1.amazonaws.com', 'ec2-54-242-129-207.compute-1.amazonaws.com', 'ec2-54-225-22-165.compute-1.amazonaws.com', 'ec2-50-16-144-197.compute-1.amazonaws.com']
 #PRIVATE_TO_PUBLIC_TABLE = {'10.198.129.131':'ec2-54-225-2-34.compute-1.amazonaws.com', '10.210.205.29':'ec2-54-242-129-207.compute-1.amazonaws.com', '10.96.37.25':'ec2-54-225-22-165.compute-1.amazonaws.com', '10.96.245.71':'ec2-50-16-144-197.compute-1.amazonaws.com'}
-MASTER_NODE = ['192.168.10.144']
-SLAVE_NODE = ['192.168.10.144'] 
-SINKER_NODE = ['192.168.10.144']
-DATA_NODE = ['192.168.10.144']
-#DATA_NODE = ['10.215.197.55', '10.200.251.252', '10.206.118.135', '10.192.169.57']
-#SINKER_NODE = ['10.248.127.58']
+#MASTER_NODE = ['192.168.10.144']
+#SLAVE_NODE = ['192.168.10.144'] 
+#SINKER_NODE = ['192.168.10.144']
+#DATA_NODE = ['192.168.10.144']
+DATA_NODE = ['10.215.197.55', '10.200.251.252', '10.206.118.135', '10.192.169.57']
+SINKER_NODE = ['10.248.127.58']
 
 
 MASTER_PORT = 12535
