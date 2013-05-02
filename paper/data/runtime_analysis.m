153a49f58a0caacd33980951b729fb3970c60178
Jilong Liao <jliao2@utk.edu>
2013-05-01 21:13:46 -0400

    experiment done.

--- paper/data/runtime_analysis.m
+++ paper/data/runtime_analysis.m
@@ -1,23 +1,51 @@
 clear all;
-slave1 = load('stat_slave_1.out');
-slave2 = load('stat_slave_2.out');
-slave3 = load('stat_slave_3.out');
-slave4 = load('stat_slave_4.out');
+slave1 = load('simple/stat_slave_1_1.out');
+slave2 = load('simple/stat_slave_2_1.out');
+slave3 = load('simple/stat_slave_3_1.out');
+slave4 = load('simple/stat_slave_4_1.out');
+simple = [slave1(2500:length(slave1),:); 
+          slave2(2500:length(slave2),:); 
+          slave3(2500:length(slave3),:); 
+          slave4(2500:length(slave4),:)];
+runtime1 = simple(:,1) - simple(:,2);
 
-runtime1 = slave1(:,1) - slave1(:,2);
-runtime2 = slave2(:,1) - slave2(:,2);
-runtime3 = slave3(:,1) - slave3(:,2);
-runtime4 = slave4(:,1) - slave4(:,2);
+slave1 = load('cloud-cache/stat_slave_1_1.out');
+slave2 = load('cloud-cache/stat_slave_2_1.out');
+slave3 = load('cloud-cache/stat_slave_3_1.out');
+slave4 = load('cloud-cache/stat_slave_4_1.out');
+ccache = [slave1; slave2; slave3; slave4];
+runtime2 = ccache(:,1) - ccache(:,2);
 
+[r1, x1] = ecdf(runtime1);
+[r2, x2] = ecdf(runtime2);
 
-[r1, x1] = ecdf(runtime1*1000);
-[r2, x2] = ecdf(runtime2*1000);
-[r3, x3] = ecdf(runtime3*1000);
-[r4, x4] = ecdf(runtime4*1000);
 
 figure(1); hold on;
 plot(x1, r1, '-r', 'linewidth', 2);
 plot(x2, r2, '-b', 'linewidth', 2);
-plot(x3, r3, '-g', 'linewidth', 2);
-plot(x4, r4, '-m', 'linewidth', 2);
-axis([0 1000 0 1]);
+axis([0 10 0 1]);
+grid on;
+hold off;
+
+% slave = sortrows(simple, 1);
+% length(slave)
+% 
+% task = [0];
+% start = 1367334283;
+% k = 1
+% t = 0
+% for i = 1:length(slave)
+%     if slave(i,1) < start + 300
+%         task(k) = task(k) + (slave(i,1) - slave(i,2));
+%         t = t + 1;
+%     else
+%         task(k) = task(k)/t;
+%         t = 0;
+%         start = start + 300;
+%         k = k + 1;
+%         task = [task 0];
+%     end
+% end
+% figure(2);
+% plot(1:length(task), task)
+% axis([0 length(task)-10 0 0.5])
---
+++ paper/data/simple/retrieve.sh
@@ -0,0 +1,11 @@
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-224-241-85.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/stat.out stat_slave_1_1.out
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-224-241-85.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/cache.out cache_slave_1_1.out
+
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-225-29-78.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/stat.out stat_slave_2_1.out
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-225-29-78.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/cache.out cache_slave_2_1.out
+
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-224-234-78.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/stat.out stat_slave_3_1.out
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-224-234-78.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/cache.out cache_slave_3_1.out
+
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-243-6-222.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/stat.out stat_slave_4_1.out
+scp -i ~/.ec2/amazon-ubuntu.pem ubuntu@ec2-54-243-6-222.compute-1.amazonaws.com:/home/ubuntu/cloud-cache/src/external/cache.out cache_slave_4_1.out
